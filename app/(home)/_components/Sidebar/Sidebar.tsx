6e0a0a8bfee1c0e62e4a7932762d5ddfdc4fc987
Thomas DOUCHE <Thomas.douche@aol.com>
2024-11-21 20:11:07 +0100

    implement data fetching

--- app/(home)/_components/Sidebar/Sidebar.tsx
+++ app/(home)/_components/Sidebar/Sidebar.tsx
@@ -1,31 +1,42 @@
+/**
+ * The Sidebar is composed of a logo, a search bar, and a list of search results, or potential error messages.
+ * It allows the user to search for movies and select one to view its details.
+ */
+
 'use client';
-import { mockMovieResults } from "./mock-data";
 import styles from "./sidebar.module.css";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Search, Clapperboard, LoaderCircle } from "lucide-react";
+import searchMovies from "@/app/api/search-movies/wrapper";
+import adaptMovieSearchResponse from "./adapter";
+import { useState } from "react";
 
 
+/** The data displayed in a row of the list of results */
 export interface MovieResult {
     id: number;
     title: string;
-    release_date: Date;
+    release_date?: Date;
 }
+
+type Status =
+    | 'initial' // Initial state of the sidebar. No search has been made yet
+    | 'idle'    // Normal state
+    | 'loading' // Fetching data is in progress
+    | 'error';  // Error fetching data
 
 interface SidebarViewProps {
-    status: 'idle' | 'loading' | 'error';
+    status: Status;
     results: MovieResult[];
     onSearch: (query: string) => void;
-    onSelect: (id: number) => void;
-    className?: string;
-}
-
-interface SidebarProps {
-    className?: string;
 }
 
 
-function SidebarView({status, results, onSearch, className}: SidebarViewProps) {
+/**
+ * Stateless view component for the Sidebar. Only handles rendering the UI.
+ */
+function SidebarView({status, results, onSearch }: SidebarViewProps) {
     const pathname = usePathname();
 
     const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -36,7 +47,7 @@ function SidebarView({status, results, onSearch, className}: SidebarViewProps) {
     }
 
     return (
-        <div className={`${styles.sidebar} ${className}`}>
+        <div className={styles.sidebar}>
 
             {/* Logo */}
             <div className={styles.logo}>
@@ -46,20 +57,25 @@ function SidebarView({status, results, onSearch, className}: SidebarViewProps) {
 
             {/* Search bar */}
             <form onSubmit={onSubmit} className={styles.search_bar}>
-                <input type="text" name="query" placeholder="Search movies"/>
+                <input type="text" name="query" placeholder="Search movies" required/>
                 <button type="submit">
                     {status === "loading" ? <LoaderCircle size={20} className={styles.loader}/> : <Search size={20}/>}
                 </button>
             </form>
 
-            {/* Error message */}
-            {status === "error" && (
-                <p className={styles.error}>An error occurred while fetching the data.</p>
+            {/* Initial message */}
+            {status === "initial" && (
+                <Clapperboard className={styles.initial_message}/>
             )}
 
             {/* No results message */}
             {status === "idle" && results.length === 0 && (
                 <p className={styles.no_results}>No results found.</p>
+            )}
+
+            {/* Error message */}
+            {status === "error" && (
+                <p className={styles.error}>An error occurred while fetching the data.</p>
             )}
 
             {/* Results list */}
@@ -71,7 +87,7 @@ function SidebarView({status, results, onSearch, className}: SidebarViewProps) {
                         className={styles.result_link + (pathname === `/${result.id}` ? ` ${styles.active}` : "")}
                     >
                         <p className={styles.title}>{result.title}</p>
-                        <p className={styles.date}>{result.release_date.getFullYear()}</p>
+                        <p className={styles.date}>{result.release_date?.getFullYear() || "Unknown"}</p>
                     </Link>
                 ))}
             </div>
@@ -81,16 +97,33 @@ function SidebarView({status, results, onSearch, className}: SidebarViewProps) {
 }
 
 
-export default function Sidebar({className}: SidebarProps) {
-    const data = mockMovieResults;
+/**
+ * The Sidebar component fetches movie data from the TMDB API based on a search query.
+ */
+export default function Sidebar() {
+    const [status, setStatus] = useState<SidebarViewProps['status']>('initial');
+    const [data, setData] = useState<MovieResult[]>([])
+
+    const handleSearch = async (query: string) => {
+        setStatus('loading');
+        const { data, error } = await searchMovies({ query, page: 1 });
+
+        if (data && !error) {
+            setData(adaptMovieSearchResponse(data));
+            setStatus('idle');
+
+        } else {
+            setStatus('error');
+            setData([]);
+        }
+    }
+
 
     return (
         <SidebarView
-            status="idle"
+            status={status}
             results={data}
-            onSearch={() => {}}
-            onSelect={() => {}}
-            className={className}
+            onSearch={handleSearch}
         />
     )
 };
