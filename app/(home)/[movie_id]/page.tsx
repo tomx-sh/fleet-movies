35dd0bfda4a55762dc6cf59549809610fe6a2909
Thomas DOUCHE <Thomas.douche@aol.com>
2024-11-21 22:37:45 +0100

    implement movie details data fetching

--- app/(home)/[movie_id]/page.tsx
+++ app/(home)/[movie_id]/page.tsx
@@ -1,9 +1,17 @@
+import getMovieDetails from "@/app/api/get-movie-details/wrapper.server";
+
+/**
+ * This is a page showing details for a movie.
+ */
 export default async function Page({params}: {params: Promise<{movie_id: string}>}) {
     const movieId = (await params).movie_id;
+    const { data, error } = await getMovieDetails({ id: parseInt(movieId) });
+    if (error) throw new Error(error); // Will show the error page
+
     return (
-        <div>
-            <p>Movie</p>
-            <p>{movieId}</p>
-        </div>
+        <main>
+            <h1>{data?.title}</h1>
+            <p>{data?.overview}</p>
+        </main>
     );
 }
