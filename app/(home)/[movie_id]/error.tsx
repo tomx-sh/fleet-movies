13a0494d203d40fdc54dd8d2074490f0daf43b7f
Thomas DOUCHE <Thomas.douche@aol.com>
2024-11-21 22:35:36 +0100

    add error page

---
+++ app/(home)/[movie_id]/error.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+
+/**
+ * This is the error page for when a movie is not found,
+ * or something went wrong.
+ */
+export default function Error() {
+    return (
+        <main>
+            <h1>Movie not found</h1>
+            <p>The movie you are looking for does not exist.</p>
+        </main>
+    );
+}
