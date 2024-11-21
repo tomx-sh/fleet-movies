5790cb163632b966d019e238c1cfa6d229c7ffb0
Thomas DOUCHE <Thomas.douche@aol.com>
2024-11-21 22:37:31 +0100

    nice welcome home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,14 @@
+import { Clapperboard } from "lucide-react";
+import styles from "./page.module.css";
+
+/**
+ * This page is the default content for the home route.
+ */
 export default function Home() {
     return (
-        <main>
-            <p>Hello</p>
+        <main className={styles.main}>
+            <Clapperboard size={40} />
+            <h1>Welcome to the Fleet Movie Database</h1>
         </main>
     );
 }
